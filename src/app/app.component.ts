4e54eaba6409028173d51fa9e0361d878b217a2f
ssadown <bibatututu@yandex.ru>
2024-11-11 22:15:18 +0500

    Сделал компонет доски

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    BoardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'atomic-knight';
+  main: any;
 }
